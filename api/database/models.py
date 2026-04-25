6346fa23d0ee474bdbbca758022752cf2edd30a2
Kritos <artemsoklakov2@gmail.com>
2026-04-25 19:18:48 +0300

    feat: added PC database model, applied indexing on User model on tg_id column

--- api/database/models.py
+++ api/database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import BigInteger, String, Boolean, DateTime, func
+from sqlalchemy import BigInteger, Integer, String, Boolean, DateTime, func
 from datetime import datetime
 
 class Base(DeclarativeBase):
@@ -8,9 +8,16 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
-    tg_id: Mapped[int | None] = mapped_column(BigInteger, unique=True)
+    tg_id: Mapped[int | None] = mapped_column(BigInteger, unique=True, index = True)
     username: Mapped[str | None] = mapped_column(String(255), unique=True)
     full_name: Mapped[str | None] = mapped_column(String(255))
     hashed_password: Mapped[str | None] = mapped_column(String(255))
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+
+class PC(Base):
+    __tablename__ = "pcs"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name : Mapped[str] = mapped_column(String(255), unique=True, index = True)
+    price_per_hour : Mapped[int] = mapped_column(Integer)
+    status : Mapped[str] = mapped_column(String(20), default='free')
